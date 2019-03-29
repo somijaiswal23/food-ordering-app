294fc2e03876eac438f4af5fb866a06eabf6dd59
Somi Jaiswal <42111833+somijaiswal23@users.noreply.github.com>
2019-03-29 20:08:12 +1100

    dummy json

---
+++ src/screens/details/dummy.js
@@ -0,0 +1,196 @@
+export default {
+    "id": "3097b8f4-a294-11e8-9a3a-720006ceb890",
+    "restaurant_name": "Loud Silence",
+    "photo_URL": "https://b.zmtcdn.com/data/reviews_photos/94a/be67cc20a6ab663f95330e5af6afb94a_1521359398.jpg",
+    "customer_rating": 4.4,
+    "average_price": 600,
+    "number_customers_rated": 658,
+    "address": {
+      "id": "f114b346-a237-11e8-9077-720006ceb890",
+      "flat_building_name": "K Star Hotel, Plot 46 & 55",
+      "locality": "CBD-Belapur",
+      "city": "Navi Mumbai",
+      "pincode": "400102",
+      "state": {
+        "id": "c860e78a-a29b-11e8-9a3a-720006ceb890",
+        "state_name": "Maharashtra"
+      }
+    },
+    "categories": [
+      {
+        "id": "2ddf5e9c-ecd0-11e8-8eb2-f2801f1b9fd1",
+        "category_name": "Chinese",
+        "item_list": [
+          {
+            "id": "e23700a3-cd04-4736-8212-96d9cc07f322",
+            "item_name": "Chicken Wrap",
+            "price": 245,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf307a-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chocolate icecream",
+            "price": 240,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf4ace-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "hakka noodles",
+            "price": 204,
+            "item_type": "NON_VEG"
+          }
+        ]
+      },
+      {
+        "id": "2ddf6158-ecd0-11e8-8eb2-f2801f1b9fd1",
+        "category_name": "Continental",
+        "item_list": [
+          {
+            "id": "e23700a3-cd04-4736-8212-96d9cc07f322",
+            "item_name": "Chicken Wrap",
+            "price": 245,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf5e9c-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Peri-peri chicken",
+            "price": 280,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "d715446a-f936-435b-b578-17234f9a013e",
+            "item_name": "Portuguese Salad",
+            "price": 245,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf4f88-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chicken biryani",
+            "price": 245,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf307a-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chocolate icecream",
+            "price": 240,
+            "item_type": "VEG"
+          }
+        ]
+      },
+      {
+        "id": "2ddf567c-ecd0-11e8-8eb2-f2801f1b9fd1",
+        "category_name": "Indian",
+        "item_list": [
+          {
+            "id": "e23700a3-cd04-4736-8212-96d9cc07f322",
+            "item_name": "Chicken Wrap",
+            "price": 245,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf63b0-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Espetada Carnival",
+            "price": 204,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "d715446a-f936-435b-b578-17234f9a013e",
+            "item_name": "Portuguese Salad",
+            "price": 245,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf567c-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Spicy Mixed Olives",
+            "price": 300,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf3d04-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chicken chillie",
+            "price": 300,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf43ee-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chillie paneer",
+            "price": 280,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf3868-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "pasta",
+            "price": 270,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf2904-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "tea",
+            "price": 20,
+            "item_type": "VEG"
+          }
+        ]
+      },
+      {
+        "id": "2ddf5546-ecd0-11e8-8eb2-f2801f1b9fd1",
+        "category_name": "Italian",
+        "item_list": [
+          {
+            "id": "2ddf4ace-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "hakka noodles",
+            "price": 204,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf2454-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "pizza",
+            "price": 300,
+            "item_type": "VEG"
+          }
+        ]
+      },
+      {
+        "id": "2ddf57a8-ecd0-11e8-8eb2-f2801f1b9fd1",
+        "category_name": "Snacks",
+        "item_list": [
+          {
+            "id": "2ddf63b0-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Espetada Carnival",
+            "price": 204,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf5e9c-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Peri-peri chicken",
+            "price": 280,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf567c-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "Spicy Mixed Olives",
+            "price": 300,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf3d04-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chicken chillie",
+            "price": 300,
+            "item_type": "NON_VEG"
+          },
+          {
+            "id": "2ddf43ee-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "chillie paneer",
+            "price": 280,
+            "item_type": "VEG"
+          },
+          {
+            "id": "2ddf3868-ecd0-11e8-8eb2-f2801f1b9fd1",
+            "item_name": "pasta",
+            "price": 270,
+            "item_type": "VEG"
+          }
+        ]
+      }
+    ]
+  }
+  
